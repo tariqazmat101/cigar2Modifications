6a095f19837dfe6baac49e0e6b11add335519788
tariq <kapooky101@gmail.com>
2020-07-26 11:25:28 -0400

    changed relative paths to absolute paths, webpack is retarded

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "./dist/"
+        publicPath: "/assets/"
     },
     module: {
         rules: [
